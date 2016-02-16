b5de11282cb8118d6a6df883b49ae2a8766ec41b
Ajitsy <sanghera.p@gmail.com>
2016-02-16 20:23:21 +0000

    colour of thermostat temperature changes depeneding on usage

--- src/interface.js
+++ src/interface.js
@@ -5,14 +5,31 @@ $(document).ready(function(){
   $('#temperature-up').click(function() {
     thermostat.up();
     updateTemperature();
-  })
+  });
 
   $('#temperature-down').click(function() {
     thermostat.down();
     updateTemperature();
-  })
+  });
+
+  $('#temperature-reset').click(function() {
+    thermostat.reset();
+    updateTemperature();
+  });
+
+  $('#powersaving-on').click(function() {
+    thermostat.powerSaveOn();
+    updateTemperature();
+  });
+
+  $('#powersaving-off').click(function() {
+    thermostat.powerSaveOff();
+    updateTemperature();
+  });
 
   function updateTemperature() {
     $('#temperature').text(thermostat.temperature);
-  };
-})
+    $('#temperature').css("color", thermostat.colour);
+  }
+
+});
