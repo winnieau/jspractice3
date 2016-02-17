91a72cbfc650ec0ab87edd12878ffec7e5704aeb
Ajitsy <sanghera.p@gmail.com>
2016-02-17 20:58:07 +0000

    added ajax code for working with openweathermap api data

--- src/interface.js
+++ src/interface.js
@@ -32,13 +32,36 @@ $(document).ready(function(){
     $('#temperature').css("color", thermostat.colour);
   }
 
-  $('#select-city').submit(function(event) {
-    event.preventDefault();
-    var city = $('#current-city').val();
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&APPID=f46b9c5aef58799f2f61b6c7f003f5e8', function(data) {
-      $('#current-temp').text(data.main.temp);
-    });
 
+  $('#select-city').submit(function(event) {
+    // Fetch the data from the public API through JSONP.
+    // See http://openweathermap.org/API#weather.
+    event.preventDefault();
+
+    var city = $('#current-city').val();
+
+    $.ajax({
+      url: 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&APPID=f46b9c5aef58799f2f61b6c7f003f5e8',
+      jsonp: 'callback',
+      dataType: 'jsonp',
+      cache: false,
+      data: {
+        q: $('#current-city').val(),
+      },
+      // work with the response
+      success: function (data) {
+        $('#current-temp').text(data.main.temp);
+      }
+    });
   });
+
+  // $('#select-city').submit(function(event) {
+  //   event.preventDefault();
+  //   var city = $('#current-city').val();
+  //   $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&APPID=f46b9c5aef58799f2f61b6c7f003f5e8', function(data) {
+  //     $('#current-temp').text(data.main.temp);
+  //   });
+  //
+  // });
 
 });
